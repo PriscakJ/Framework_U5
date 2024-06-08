e08a7777737012d59b4e645b0caaf9ab07f0e3f7
juraj <priscak.juraj@gmail.com>
2024-06-08 22:54:16 +0200

    login and signup UI done

---
+++ my-app/src/app/signup/signup.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+}
